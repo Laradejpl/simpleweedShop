ea6eab2640e923c0b2ac7bce64bcc25427c50dea
laradejpl <51596185+Laradejpl@users.noreply.github.com>
2021-02-20 22:57:07 +0100

    mise en place de l'activity products avec la bdd

---
+++ app/src/main/java/cereal/company/weedsimple/CartProductsActivity.kt
@@ -0,0 +1,11 @@
+package cereal.company.weedsimple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartProductsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_products)
+    }
+}
--- app/src/main/java/cereal/company/weedsimple/FetchEproductsActivity.kt
+++ app/src/main/java/cereal/company/weedsimple/FetchEproductsActivity.kt
@@ -1,11 +1,76 @@
 package cereal.company.weedsimple
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_fetch_eproducts.*
+import kotlinx.android.synthetic.main.header_menu.*
+import kotlinx.android.synthetic.main.side_menu_nav.*
 
 class FetchEproductsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fetch_eproducts)
+
+
+        backarrow_fetch_product.setOnClickListener {
+
+            finish()
+        }
+
+        backhome.setOnClickListener {
+
+            val intent = Intent(this@FetchEproductsActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val selectedBrand = intent.getStringExtra("BRAND")
+        title_brand_text_view.text = "$selectedBrand"
+
+        var productsList = ArrayList<EProduct>()
+        var productsUrl = "https://reggaerencontre.com/fetch_eproducts.php?brand=$selectedBrand"
+        val requestQ: RequestQueue = Volley.newRequestQueue(this)
+        var jsonAR = JsonArrayRequest(Request.Method.GET, productsUrl,null, {
+                response ->
+
+            for (productJOIndex in 0.until(response.length())){
+
+
+                productsList.add(EProduct(response.getJSONObject(productJOIndex).getInt("id") , response.getJSONObject(productJOIndex).getString("name"),
+                    response.getJSONObject(productJOIndex).getInt("price"),response.getJSONObject(productJOIndex).getString("picture") ))
+
+            }
+
+
+            val pAdapter = EProductAdapter(this@FetchEproductsActivity, productsList)
+            productsRV.layoutManager = LinearLayoutManager(this@FetchEproductsActivity)
+            productsRV.adapter = pAdapter
+
+
+        }, { error ->
+
+
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setTitle("Message")
+            dialogBuilder.setMessage(error.message)
+            dialogBuilder.create().show()
+
+
+
+
+
+
+
+        })
+
+        requestQ.add(jsonAR)
     }
 }
