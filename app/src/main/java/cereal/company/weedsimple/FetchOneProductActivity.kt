6d6ac0b5b74d925884219c2bd04b994fe8e5c24c
laradejpl <51596185+Laradejpl@users.noreply.github.com>
2021-02-21 20:08:34 +0100

    ajout des champs description et picture 1

--- app/src/main/java/cereal/company/weedsimple/FetchOneProductActivity.kt
+++ app/src/main/java/cereal/company/weedsimple/FetchOneProductActivity.kt
@@ -1,5 +1,6 @@
 package cereal.company.weedsimple
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,13 +11,33 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
+import kotlinx.android.synthetic.main.activity_fetch_eproducts.*
 import kotlinx.android.synthetic.main.activity_fetch_one_product.*
+import kotlinx.android.synthetic.main.activity_fetch_one_product.backarrow_fetch_product
+import kotlinx.android.synthetic.main.activity_fetch_one_product.backhome
 import kotlinx.android.synthetic.main.activity_sign_up_layout.*
 
 class FetchOneProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fetch_one_product)
+
+
+        backarrow_fetch_product.setOnClickListener {
+
+            finish()
+        }
+
+        backhome.setOnClickListener {
+
+            val intent = Intent(this@FetchOneProductActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
         var picURL = "https://reggaerencontre.com/"
 
         var rq: RequestQueue = Volley.newRequestQueue(this)
@@ -28,11 +49,15 @@ class FetchOneProductActivity : AppCompatActivity() {
                 { response ->
 
 
-
+                    title_product_details_tv.text=response.getString("name")
                     name_product_fetchone_tv.text=response.getString("name")
+                    description_product_tv.text=response.getString("description")
 
                     price_product_fetchone_tv.text=response.getInt("price").toString()
                     Picasso.with(this).load(picURL + response.getString("picture")).into(image_du_produit)
+
+                    Picasso.with(this).load(picURL + response.getString("picture1")).into(image_du_produit1)
+
 
 
 
